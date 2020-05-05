68cf37068ff87099d83903348f319641b7af4bfb
EwanMcNeil <ewanmcneil11@gmail.com>
2020-05-04 20:53:09 -0400

    Added in turtle Game with two algos

--- Algorithms in python/MazeRunner/mazeLHR.py
+++ Algorithms in python/MazeRunner/mazeLHR.py
@@ -3,7 +3,7 @@
 # maze solving program using the Left Hand Rule algorithm
 # DavisMT 07.01.2018
 ##########################################################
-
+from collections import defaultdict
 import turtle                    # import turtle library
 import time
 import sys
@@ -47,6 +47,7 @@ class sprite(turtle.Turtle):
             x_walls = round(sprite.xcor(),0)          # sprite x coordinates =
             y_walls = round(sprite.ycor(),0)
             if (x_walls, y_walls) in finish:          # if the sprite is on tha
+                print("lefthand rule count: " + str(LHRcount))
                 print("Finished")
                 endProgram()
             if (x_walls +24, y_walls) in walls:          # check to see if they are walls on the left
@@ -64,6 +65,7 @@ class sprite(turtle.Turtle):
             x_walls = round(sprite.xcor(),0)
             y_walls = round(sprite.ycor(),0)
             if (x_walls, y_walls) in finish:   # check turtle coordinates are at the finish line
+                print("lefthand rule count: " + str(LHRcount))
                 print("Finished")
                 endProgram()
             if (x_walls, y_walls +24) in walls:       # check to see if they are walls on the left
@@ -81,6 +83,7 @@ class sprite(turtle.Turtle):
             x_walls = round(sprite.xcor(),0)
             y_walls = round(sprite.ycor(),0)
             if (x_walls, y_walls) in finish:   # check turtle coordinates are at the finish line
+                print("lefthand rule count: " + str(LHRcount))
                 print("Finished")
                 endProgram()
             if (x_walls -24, y_walls ) in walls:  # check to see if they are walls on the left
@@ -98,6 +101,7 @@ class sprite(turtle.Turtle):
             x_walls = round(sprite.xcor(),0)
             y_walls = round(sprite.ycor(),0)
             if (x_walls, y_walls) in finish:   # check turtle coordinates are at the finish line
+                print("lefthand rule count: " + str(LHRcount))
                 print("Finished")
                 endProgram()
             if (x_walls, y_walls -24) in walls:  # check to see if they are walls on the left
@@ -108,6 +112,68 @@ class sprite(turtle.Turtle):
             else:
                 self.left(90)
                 self.forward(24)
+
+
+class Graph:
+
+    def __init__(self):
+        
+
+        #default dictonaily to store the graph
+        self.graph = defaultdict(list)
+
+    def addEdge(self,u,v):
+        self.graph[u].append(v)
+    
+    def addcoordtoNode(self,u,x,y):
+        self.graph[u].x = x
+        self.graph[u].y = y
+
+
+
+
+
+    #function to make a breadth first seach of the graph
+
+    def BFS(self,s,endNode):
+
+        BFScount = 0
+        #mark all the vertices not visited
+        visited = [False] * (10000) 
+
+        queue = []
+        #mark the source node as visted and enqueue it
+
+        queue.append(s)
+        visited[s]= True
+
+        while queue:
+
+            #dequeue a vertex from the queue and print it
+            s = queue.pop(0)
+            
+            BFSqueue.append(s)
+            print(s, end = " ")
+            BFScount += 1
+
+            if s == endNode:                ## leaves if the end node is found
+                break
+
+
+            #get all adjacent vertives of the dequeued vertex s
+            #if adjacent has not been vistited then mark it and enqueue it
+
+            #goes through all the nodes connected to it
+            for i in self.graph[s]:
+                if i != 0:
+                    print("loop index" + str(i))
+                    if visited[i] == False: 
+                       
+                        queue.append(i)
+                        visited[i] = True
+        print("number of moves BFS had to make: " + str(BFScount))
+
+
 
 
 def endProgram():
@@ -139,9 +205,18 @@ grid = [
 ]
 
 
+
 def setupMaze(grid):
+
+
+
+
     for y in range(len(grid)):                       # select each line in the grid
         for x in range(len(grid[y])):                # identify each character in the line
+
+                                                    
+
+
             character = grid[y][x]                   # assign the grid reference to the variable character
             screen_x = -588 + (x * 24)               # assign screen_x to screen starting position for x ie -588
             screen_y = 288 - (y * 24)                # assign screen_y to screen starting position for y ie  288
@@ -158,22 +233,103 @@ def setupMaze(grid):
 
             if character == "s":                     # if the grid character contains an s
                 sprite.goto(screen_x, screen_y)      # move turtle to the x and y location
+                spriteBFS.goto(screen_x,screen_y)
+
+
+def gridToGraph(grid,g):
+
+
+
+
+    for y in range(len(grid)):                       # select each line in the grid
+        for x in range(len(grid[y])): 
+
+            character = grid[y][x]                   # assign the grid reference to the variable character
+            screen_x = -588 + (x * 24)               # assign screen_x to screen starting position for x ie -588
+            screen_y = 288 - (y * 24)
+            node = x*y
+
+            if character == "s":
+                 startNode = x*y
+                 g.addcoordtoNode(node,x,y)
+                 if (screen_x - 24,screen_y) not in walls:
+                    g.addEdge(x*y,(x-1)*y)
+                 if (screen_x + 24, screen_y) not in walls:
+                    g.addEdge(x*y,(x+1)*y)
+                 if (screen_x, screen_y-24) not in walls:
+                    g.addEdge(x*y,(y-1)*x)
+                 if (screen_x, screen_y+24) not in walls:
+                    g.addEdge(x*y,(y+24)*x)  
+
+            if character == " ":
+                 g.addcoordtoNode(node,x,y)
+                 if (screen_x - 24,screen_y) not in walls:
+                    g.addEdge(x*y,(x-1)*y)
+                 if (screen_x + 24, screen_y) not in walls:
+                    g.addEdge(x*y,(x+1)*y)
+                 if (screen_x, screen_y-24) not in walls:
+                    g.addEdge(x*y,(y-1)*x)
+                 if (screen_x, screen_y+24) not in walls:
+                    g.addEdge(x*y,(y+24)*x)
+            if character == "e":
+                endNode = x*y
+
+
+
+
+class spriteBFS(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+        self.shape("turtle")
+        self.color("green")
+        self.setheading(270)  # point turtle to point down angle has been predefined
+        self.penup()
+        self.speed(0)
+    
+    def nextMove(self,nodeNumber):
+        x = g.graph[nodeNumber].x
+        y = g.graph[nodeNumber].y
+        screen_x = -588 + (x * 24)               # assign screen_x to screen starting position for x ie -588
+        screen_y = 288 - (y * 24)
+
+        spriteBFS.goto(screen_x,screen_y)
+
+
+
+
+
+
+
+
 
 
 # ############ main program starts here  ######################
-
+g = Graph()
 maze = Maze()                # enable the maze class
 sprite = sprite()            # enable the sprite  class
+spriteBFS = spriteBFS()
 end = End()                  # enable End position class
 walls =[]                    # create walls coordinate list
 finish = []                  # enable the finish array
+BFSqueue = []
 
+gridToGraph(grid,g)
 setupMaze(grid)              # call the setup maze function
+
+LHRcount = 0
+startNode = 0
+endNode = 0
+
+g.BFS(startNode,endNode)
 
 while True:
         sprite.spriteRight()
         sprite.spriteDown()
         sprite.spriteleft()
         sprite.spriteUp()
+        LHRcount += 1
 
-        time.sleep(0.1)
+        
+        spriteBFS.nextMove(BFSqueue.pop(0))
+
+        time.sleep(0.01)
