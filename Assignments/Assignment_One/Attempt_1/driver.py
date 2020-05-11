dbd4278dca0463fba6f5924a330924ae03b1126b
EwanMcNeil <ewanmcneil11@gmail.com>
2020-05-11 00:14:59 -0400

    Not sure I should use the graph im trying to bulid at the moment

--- Assignments/Assignment_One/Attempt_1/driver.py
+++ Assignments/Assignment_One/Attempt_1/driver.py
@@ -56,6 +56,75 @@ print("the number of crimes within bounds is: " + str(output))
 
 
 
+#trying to build the graph to both display the blocks
+#but also be the basis for the A*
+
+
+class Graph:
+
+    def __init__(self):
+        # default dictonaily to store the graph
+        self.graph = dict()
+
+    # edges are appended right to it and added if not existing
+    def addEdge(self, u, v):
+        if u not in self.graph:
+            print(True)
+            self.graph[u] = [v]
+        else:
+            print(False)
+            self.graph[u].append(v)
+        if v not in self.graph:
+            print(True)
+            self.graph[v] = [u]
+        else:
+            print(False)
+            self.graph[v].append(u)
+
+    # coords are appended after
+    def addinfo(self,count,xBot, xTop, yBot, yTop, crimeNumber):
+        self.graph[count] = [crimeNumber]
+        coords = (xBot, xTop, yBot, yTop)
+        self.graph[count].append(coords)
+
+    def getLength(self):
+        return len(self.graph)
+
+
+
+
+
+#size is the multipler so I think as you go along you 
+#add a bit each time
+def graphCreation(size):
+    graph = Graph()
+
+    xBot = -73.59
+    xTop = -73.59+size
+    yBot = 45.49
+    yTop = 45.49 +size
+
+    #count gives an index for each box
+    count = 1
+
+    while(True):
+        crime = crimesWithinBounds(xBot,xTop,yBot,yTop)
+        graph.addinfo(count,xBot, xTop, yBot, yTop, crime)
+        xBot += size
+        xTop += size
+        yBot += size
+        yTop += size
+        count += 1
+        if(xTop >= -73.55):
+            return graph
+
+
+
+print(graphCreation(0.002).getLength)
+
+
+    
+    
 
 
 
